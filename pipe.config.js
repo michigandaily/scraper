0f8f2681f98897fe79a955c0abad62a2e8b48be3
yumarie <yumarie@umich.edu>
2026-04-26 03:14:53 -0400

    update config standards

--- pipe.config.js
+++ pipe.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "piping-bag";
+import { defineConfig, defineSchedulerDate } from "piping-bag";
 
 export default defineConfig({
   deployment: {
@@ -7,5 +7,20 @@ export default defineConfig({
     // path: "./tmp/scraper.zip",
     zip_dir: "./tmp",
     profile: "pipe",
+  },
+  schedule: {
+    start: defineSchedulerDate({
+      hour: 9, // 9 AM
+      day: 1, // 1st
+      month: 1, // January
+      year: 2027,
+    }),
+    end: defineSchedulerDate({
+      hour: 21, // 9 PM
+      day: 2, // 2nd
+      month: 1, // January
+      year: 2027,
+    }),
+    rate: "rate(5 minutes)",
   },
 });
